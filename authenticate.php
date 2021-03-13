7a0296cbe0743d227c657c9c45771261e4406277
junny1117 <80441056+junny1117@users.noreply.github.com>
2021-03-13 02:30:31 +0000

    Update authenticate.php

--- authenticate.php
+++ authenticate.php
@@ -3,8 +3,8 @@ session_start();
 // Change this to your connection info.
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
-$DATABASE_PASS = 'root_password';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'db7gx4qvsdzaqf';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if ( mysqli_connect_errno() ) {
